28b88178c4a51142ee4b85a309907296394a2de2
apple <apple@192.168.0.102>
2019-09-26 20:02:40 +0530

    update

---
+++ DataStructureAndAlgorithms/src/com/lav/sort/InsertionSort.java
@@ -0,0 +1,42 @@
+package com.lav.sort;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		int[] array = { 3, 5, 7, 5, 1, 9, 2 };
+
+		InsertionSort(array);
+
+		for (int num : array) {
+			System.out.print(num + ", ");
+		}
+
+	}
+
+	public static void InsertionSort(int[] array) {
+
+		for (int i = 0; i < array.length - 1; i++) {
+
+			int minValueIndex = i;
+
+			for (int j = i + 1; j < array.length; j++) {
+
+				if (array[j] < array[minValueIndex]) {
+					minValueIndex = j;
+				}
+
+				swap(array, i, minValueIndex);
+			}
+
+		}
+
+	}
+
+	public static void swap(int[] array, int i, int j) {
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+
+}
