5cb99f510cb15e5f777e98f2b02e9dda898f7efa
apple <apple@192.168.0.102>
2019-09-26 20:45:49 +0530

    Quick sort done

--- DataStructureAndAlgorithms/src/com/lav/sort/QuickSort.java
+++ DataStructureAndAlgorithms/src/com/lav/sort/QuickSort.java
@@ -2,15 +2,15 @@ package com.lav.sort;
 
 public class QuickSort {
 
-	/*- Algorithm works on divide and conquer
-	 *  pivot point which is in between of smaller elements
-	 *  on left and larger elements on right  
+	/*- Best sorting algorithm with time complexity of 
+	 * 
+	 *  O(n log n)
 	 */
 
 	public static void main(String[] args) {
 		int[] array = { 3, 5, 7, 5, 1, 9, 2 };
 
-		quickSort(array);
+		quickSort(array, 0, array.length - 1);
 
 		for (int num : array) {
 			System.out.print(num + ", ");
@@ -18,7 +18,39 @@ public class QuickSort {
 
 	}
 
-	public static void quickSort(int[] array) {
+	public static void quickSort(int[] array, int start, int end) {
 
+		if (start >= end) {
+			return;
+		} else {
+			int partitionIndex = partitioning(array, start, end);
+			quickSort(array, start, partitionIndex - 1);
+			quickSort(array, partitionIndex + 1, end);
+		}
+
+	}
+
+	public static int partitioning(int[] array, int start, int end) {
+
+		int pivot = array[end]; // we are taking last element as pivotal
+		
+		int partitionIndex = start;
+
+		for (int i = start; i < end - 1; i++) {
+			if (array[i] <= pivot) {
+				swap(array, i, partitionIndex);
+				partitionIndex++;
+			}
+		}
+
+		swap(array, partitionIndex, end);
+		return partitionIndex;
+
+	}
+
+	public static void swap(int[] array, int i, int j) {
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
 	}
 }
